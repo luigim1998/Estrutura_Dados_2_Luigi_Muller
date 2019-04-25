07537379ad561c82745c8979dcf307a0206620a4
Unknown <sluigimuller@gmail.com>
2019-04-25 10:15:54 -0400

    Editando a AVL
    Verificado a inserção e remoção, falta a impressão e busca.

--- AVL/AVL.h
+++ AVL/AVL.h
@@ -12,12 +12,16 @@ typedef struct arv_avl{
     Nodo * raiz;
 }Avl;
 
+Avl * cria_arv();
+void arv_inserir(Avl * arv, int k);
 Nodo * inserir(Nodo * raiz, int k);
 int altura(Nodo * p);
 int max(int a, int b);
 Nodo * novo_nodo(int info);
-Nodo * dirRotate(Nodo * y);
+Nodo * dirRotate(Nodo * x);
 Nodo * esqRotate(Nodo * x);
 int calcular_fator_balanceamento(Nodo * r);
+void arv_remover(Avl * arv, int k);
 Nodo * remover(Nodo * raiz, int k);
+
 #endif //AVL_AVL_H
--- AVL/main.cpp
+++ AVL/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "AVL.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(){
+    Avl * arv = cria_arv();
+    arv_inserir(arv,  50);
+    arv_inserir(arv,  25);
+    arv_inserir(arv,  75);
+    arv_inserir(arv,  12);
+    arv_inserir(arv,   6);
+    arv_inserir(arv,  90);
+    arv_inserir(arv, 100);
+    arv_inserir(arv, 120);
+    arv_inserir(arv, 110);
+    arv_inserir(arv,   3);
+    arv_inserir(arv,   5);
+
+    arv_remover(arv, 50);
     return 0;
 }
